a159ba7317cf67463e1a810a39e362969483f556
j-west <westja89@gmail.com>
2016-12-01 14:47:49 -0600

    Add object to store height value and buildKey value

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var btn = document.querySelector("button");
 var heightInput;
 var keyInput;
 
+var treeSpecs = {
+                  "Height" : 0,
+                  "buildKey" : 0
+}
+
 function growThatTree() {
   heightInput = document.querySelectorAll("input")[0].value;
   keyInput = document.querySelectorAll("input")[1].value;
