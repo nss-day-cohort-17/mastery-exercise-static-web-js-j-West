8c80ef31c28cc616e99fecd837142640212b36b8
j-west <westja89@gmail.com>
2016-12-01 14:29:36 -0600

    Add three variables in main.js and store html elements in them

---
+++ main.js
@@ -0,0 +1,7 @@
+var btn = document.querySelector("button");
+var heightInput = document.querySelectorAll("input")[0];
+var keyInput = document.querySelectorAll("input")[1];
+
+console.log(heightInput);
+console.log(keyInput);
+console.log(btn);
