0fe0dea707e014971f4d6380ac694ea41ac397aa
j-west <westja89@gmail.com>
2016-12-01 14:40:21 -0600

    Moved anon function from button listener to global scope so it can be called from enter event as well as button click

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var btn = document.querySelector("button");
 var heightInput;
 var keyInput;
 
-btn.addEventListener("click", function() {
+function growThatTree() {
   heightInput = document.querySelectorAll("input")[0].value;
   keyInput = document.querySelectorAll("input")[1].value;
 
   console.log(heightInput);
   console.log(keyInput);
+}
 
-})
+
+btn.addEventListener("click", growThatTree)
